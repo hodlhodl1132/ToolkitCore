bedd9fa4e495aa809dc53375cec2f772b5025394
hodlhodl <don@dmsgmail.com>
2020-03-18 13:49:47 -0700

    Fixed  .gitignore

---
+++ toolkitcore/Utilities/ViewerInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolkitCore.Controllers;
+using ToolkitCore.Models;
+using TwitchLib.Client.Models;
+using Verse;
+
+namespace ToolkitCore.Utilities
+{
+    public class ViewerInterface : TwitchInterfaceBase
+    {
+        public ViewerInterface(Game game)
+        {
+
+        }
+
+        public override void ParseCommand(ChatMessage msg)
+        {
+            ViewerController.GetViewer(msg.Username).UpdateViewerFromMessage(msg);
+        }
+    }
+}
