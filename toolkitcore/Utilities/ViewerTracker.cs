3297f1c7cdd768051d7654ec0cdb8b83f650e86e
hodlhodl <don@dmsgmail.com>
2020-04-29 02:49:25 -0700

    feat: Method  for checking when viewer was last active in chat

--- toolkitcore/Utilities/ViewerTracker.cs
+++ toolkitcore/Utilities/ViewerTracker.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToolkitCore.Models;
 
 namespace ToolkitCore.Utilities
 {
@@ -26,6 +28,18 @@ namespace ToolkitCore.Utilities
             {
                 viewersLastActiveTime.Add(Username, DateTime.Now);
             }
+        }
+
+        public static int MinutesSinceLastActive(Viewer viewer)
+        {
+            if (viewer == null || !viewersLastActiveTime.ContainsKey(viewer.Username))
+            {
+                throw new Exception("Cannot provide Minutes since viewer was last active since viewer has not been tracker.");
+            }
+
+            TimeSpan timeSpan = DateTime.Now - viewersLastActiveTime[viewer.Username];
+
+            return timeSpan.Minutes;
         }
     }
 }
